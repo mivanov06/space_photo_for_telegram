503ab728db74d4f4716ea9b7f8c3648543bbc2ba
mivanov06 <mivanov06@yandex.ru>
2023-02-26 23:47:53 +1000

    publish photo to Telegram channel

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import os
+import random
+
 import telegram
 
-
-import requests
 from dotenv import load_dotenv
+
+
+def get_random_image(folder):
+    files = os.walk(folder)
+    for root, dirs, files in files:
+        ...
+    return random.choice(files)
 
 
 if __name__ == "__main__":
     load_dotenv()
-    nasa_token = os.getenv('NASA_TOKEN')
     telegram_token = os.getenv('TELEGRAM_BOT_TOKEN')
+    chat_id = os.getenv('TELEGRAM_CHAT_ID')
     bot = telegram.Bot(telegram_token)
-    print(bot.get_me())
-    bot.send_message(chat_id='-1001680271761', text='First message')
+    folder = 'images'
+    bot.send_photo(chat_id=chat_id, photo=open(f'{folder}/{get_random_image(folder)}', 'rb'))
